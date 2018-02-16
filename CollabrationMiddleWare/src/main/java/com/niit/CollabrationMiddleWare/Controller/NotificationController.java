ae4d6bf11312be3931c59241d645c59358ea5910
AmaalSaleem <30740843+AmaalSaleem@users.noreply.github.com>
2018-02-16 10:46:16 +0530

    Project 2 for Niit

---
+++ CollabrationMiddleWare/src/main/java/com/niit/CollabrationMiddleWare/Controller/NotificationController.java
@@ -0,0 +1,69 @@
+package com.niit.CollabrationMiddleWare.Controller;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niit.CollabrationBackEnd.DAO.NotificationsDAO;
+import com.niit.CollabrationBackEnd.Model.Notifications;
+import com.niit.CollabrationBackEnd.Model.User;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+	@Autowired 
+	NotificationsDAO notificationsDAO;
+	
+	
+	
+	@RequestMapping(value="/getAllNotis",method=RequestMethod.GET,headers = "Accept=application/json")
+	public ResponseEntity<ArrayList<Notifications>> getAllNotis(HttpSession session){
+		User user=(User)session.getAttribute("currentuser");
+		System.out.println("in getall notis");
+		System.out.println(user.getEmail_id());
+		ArrayList<Notifications> notis=(ArrayList<Notifications>)notificationsDAO.getAllNotifications(user.getEmail_id());
+		for(Notifications n:notis)
+		{
+			System.err.println(n.getName());
+		}
+				return new ResponseEntity<ArrayList<Notifications>>(notis,HttpStatus.OK);
+				
+	
+	
+	
+	}
+	
+	
+	@RequestMapping(value="/deleteNoti/{notifid}",method=RequestMethod.GET)
+	public ResponseEntity<ArrayList<Notifications>> deleteNoti(@PathVariable("notifid") int notifid,HttpSession session){
+	
+	User us=(User)session.getAttribute("currentuser");
+		
+		Notifications noti=notificationsDAO.getNotifications(notifid);
+	if(notificationsDAO.deleteNotifications(noti))
+	{
+		ArrayList<Notifications> bc=notificationsDAO.getAllNotifications(us.getEmail_id());
+	
+		return new ResponseEntity<ArrayList<Notifications>>(bc,HttpStatus.OK);	
+		
+			
+		
+		
+	}
+	else
+	{
+		return null;
+	}
+	
+	
+	
+	
+	}}
